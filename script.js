5a7e8147f448aae652c20fa29e96f25d6dc0328f
Robin Abraham <robinabrahammathu@gmail.com>
2023-11-21 22:48:20 +0530

    Portfolio Major Updates Commit

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-// Add your custom JavaScript here
 $(document).ready(function () {
-    // Smooth scrolling using jQuery
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 1000);
+    $('.menu-toggler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
     });
 
-    // Activate Scrollspy
-    $('body').scrollspy({
-        target: '#navbarNav',
-        offset: 100
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').toggleClass('open');
+        $('.top-nav').toggleClass('open');
     });
 
-    // Parallax scrolling effect for the header
-    $(window).scroll(function () {
-        var scrolled = $(window).scrollTop();
-        $('.bg-primary').css('background-position-y', -scrolled * 0.3 + 'px');
+    $('nav a[href*="#]').on('click', function(){
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top-100
+        }, 2000);
+    });
+
+    $('#up').on('click', function(){
+        $('html,body').animate({
+            scrollTop: 0 } ,2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
     });
 });
